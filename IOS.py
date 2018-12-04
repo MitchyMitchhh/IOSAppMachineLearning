cb0c0b0c3ff18a2491e3e066d86fb89640d5a894
Jordan <par16042@byui.edu>
2018-12-04 15:39:39 -0700

    load dataset

--- IOS.py
+++ IOS.py
@@ -1,1 +1,6 @@
 # Test
+data = "appleStore_description.csv"
+file = pd.read_csv(data, delim_whitespace=True)
+file.columns = ['id', 'track_name', 'size_bytes', 'currency', 'price', 'rating_count_tot', 'rating_count_ver',
+                'user_rating', 'user_rating_ver', 'ver', 'cont_rating', 'prime_genre', 'sup_devices.num',
+                'ipadSc_urls.num', 'lang.num', 'vpp_lic', 'game_enab']
